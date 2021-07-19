a7c07f3950614af44cd2bc9bbff4553597afb833
Bpompa95 <brianpompa02@gmail.com>
2021-07-19 16:22:33 -0700

    quick update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     super(props)
     this.state = {
       // ACTION ITEM: Add possible Magic8 Ball answers to the array
-      answerArray: ["Here is an answer"],
+      answerArray: ["Here is an answer", "don't count on it", "yes", "no", "next year", "try again"],
       selectedAnswer: ""
     }
   }
@@ -15,7 +15,7 @@ class App extends Component {
     const { answerArray } = this.state
     // ACTION ITEM: Your code here!!
     // ACTION ITEM: Update the answerArray selection
-    this.setState({ selectedAnswer: answerArray[0] })
+    this.setState({ selectedAnswer: answerArray[Math.floor(Math.random()* 7)] })
   }
 
   render(){
